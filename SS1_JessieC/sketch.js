56000ec853b145cc76c268b32ab5b0f1075505e9
guriphasu <guriphasu@gmail.com>
2026-02-08 16:13:45 -0500

    Add files via upload

---
+++ SS1_JessieC/sketch.js
@@ -0,0 +1,73 @@
+/*
+Jessie C.
+Title: Landing On the Moon
+My concept is several things that have to do with the
+moon. (I'm also considering making future sketches
+upside down just like this one.)
+*/
+
+function setup() 
+{
+  createCanvas(1280, 720);
+
+  background(7, 59, 143);
+
+}
+
+function draw() 
+{
+  fill(230, 230, 163);
+  strokeWeight(0.5);
+  circle(640, 360, 300);
+
+  stroke(255);
+  strokeWeight(7);
+  line(640, 545, 640, 480);
+
+  fill(191, 82, 46);
+  stroke(0);
+  strokeWeight(1);
+  rect(640, 545, 80, 50);
+
+  stroke(255);
+  strokeWeight(2);
+  line(640, 550, 720, 550);
+  line(640, 558, 720, 558);
+  line(640, 566, 720, 566);
+  line(640, 574, 720, 574);
+  line(640, 582, 720, 582);
+  line(640, 590, 720, 590);
+
+  fill(70, 136, 242);
+  stroke(0);
+  strokeWeight(1);
+  rect(720, 595, -45, -27);
+
+  stroke(255);
+  strokeWeight(4.5);
+  point(715, 590);
+  point(708, 590);
+  point(701, 590);
+  point(694, 590);
+  point(688, 590);
+  point(681, 590);
+  point(715, 584);
+  point(708, 584);
+  point(701, 584);
+  point(694, 584);
+  point(688, 584);
+  point(681, 584);
+  point(715, 578);
+  point(708, 578);
+  point(701, 578);
+  point(694, 578);
+  point(688, 578);
+  point(681, 578);
+  point(715, 572);
+  point(708, 572);
+  point(701, 572);
+  point(694, 572);
+  point(688, 572);
+  point(681, 572);
+
+}
